353c771a5e4a9fa51f3531a14e7323d2061c6c30
Gustavo <gustelio97@gmail.com>
2024-10-01 01:22:25 -0300

    Se modifica codigo para cumplir lo pedido

--- js/index.js
+++ js/index.js
@@ -1,118 +1,119 @@
-// TIPOS DE COMBUSTIBLES - PRECIO
-const combustibles = [
-    {
-      tipo: "nafta",
-      eess: [
-        {
-          nombre: "ypf",
-          imagen:'./assets/eess.png',
-          categorias: [
-            { nombre: "Super", precio: 1024 },
-            { nombre: "infinia Nafta", precio: 1251 },
-          ],
-        },
-        {
-          nombre: "shell",
-          imagen:'./assets/eess.png',
-          categorias: [
-            { nombre: "Super", precio: 1085 },
-            { nombre: "vpower", precio: 1350 },
-          ],
-        },
-        {
-          nombre: "axion",
-          imagen:'./assets/eess.png',
-          categorias: [
-            { nombre: "Super", precio: 1015 },
-            { nombre: "cuantium", precio: 1120 },
-          ],
-        },
-      ],
-    },
-    {
-      tipo: "diesel",
-      eess: [
-        {
-          nombre: "ypf",
-          imagen:'./assets/eess.png',
-          categorias: [
-            { nombre: "Ultra Diesel", precio: 1114 },
-            { nombre: "infinia Diesel", precio: 1307 },
-          ],
-        },
-        {
-          nombre: "shell",
-          imagen:'./assets/eess.png',
-          categorias: [
-            { nombre: "Evolux", precio: 1150 },
-            { nombre: "Vpower Diesel", precio: 1357 },
-          ],
-        },
-        {
-          nombre: "axion",
-          imagen:'./assets/eess.png',
-          categorias: [
-            { nombre: "Diesel", precio: 1025 },
-            { nombre: "Cuantium Diesel", precio: 1250 },
-          ],
-        },
-      ],
-    },
-  ];
-// Guardar datos en localStorage
-localStorage.setItem('combustibles', JSON.stringify(combustibles));
+// Función asíncrona para cargar los combustibles
+async function cargarCombustibles() {
+  try {
+    const response = await fetch('./data/combustibles.json');
+    if (!response.ok) {
+      throw new Error('Error al cargar el archivo JSON');
+    }
+    const data = await response.json();
+    localStorage.setItem('combustibles', JSON.stringify(data.combustibles));
+  } catch (error) {
+    console.error('Error al cargar los datos con fetch', error);
+    await Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Hubo un problema al cargar los datos de combustibles!',
+      allowOutsideClick: false,
+    });
+  }
+}
+cargarCombustibles();
 
-// MUESTRA CONTENIDO TIPO COMBUSTIBLE
-document.getElementById('btnYes').addEventListener('click', () => {
-  document.getElementById('message').textContent = 'Excelente, indíquenos qué tipo de combustible usa su vehículo.';
-  document.getElementById('fuelForm').style.display = 'block'; 
-});
-
-// OCULTA FORMULARIO E INDICA GRACIAS
-document.getElementById('btnNo').addEventListener('click', () => {
-  document.getElementById('message').textContent = 'Gracias por usar nuestro servicio.';
+// Oculta formulario e indica gracias
+document.getElementById('btnNo').addEventListener('click', async () => {
+  await Swal.fire({
+    icon: 'success',
+    title: 'Gracias!',
+    text: 'Gracias por usar nuestro servicio.',
+    confirmButtonText: 'Aceptar',
+    allowOutsideClick: false,
+  });
   document.getElementById('fuelForm').style.display = 'none';
 });
 
-document.getElementById('btnCalculate').addEventListener('click', () => {
-  const combustibleSeleccionado = document.getElementById('fuelType').value.toLowerCase();
-  const monto = Number(document.getElementById('amount').value);
-  calcularLitrosPorEstaciones(combustibleSeleccionado, monto);
+// Mostrar formulario para elegir combustible
+document.getElementById('btnYes').addEventListener('click', async () => {
+  document.getElementById('fuelForm').style.display = 'block';
+  await Swal.fire({
+    title: '¡Excelente!',
+    text: 'Indíquenos qué tipo de combustible usa su vehículo.',
+    icon: 'success',
+    confirmButtonText: 'Continuar',
+    allowOutsideClick: false,
+  });
 });
 
-function calcularLitrosPorEstaciones(combustibleSeleccionado, monto) {
-  const resultDiv = document.getElementById('result');
-  const storedCombustibles = JSON.parse(localStorage.getItem('combustibles'));
-  const combustible = storedCombustibles.find((comb) => comb.tipo === combustibleSeleccionado);
+// Evento para calcular litros por estaciones
+document.getElementById('btnCalculate').addEventListener('click', async () => {
+  const combustibleSeleccionado = document.getElementById('fuelType').value.toLowerCase();
+  const monto = Number(document.getElementById('amount').value);
+  await calcularLitrosPorEstaciones(combustibleSeleccionado, monto);
+});
+
+// Función asíncrona para calcular los litros según estaciones de servicio
+async function calcularLitrosPorEstaciones(combustibleSeleccionado, monto) {
+  let storedCombustibles = localStorage.getItem('combustibles');
+  storedCombustibles = storedCombustibles ? JSON.parse(storedCombustibles) : null;
+
+  if (!storedCombustibles) {
+    await Swal.fire({
+      icon: 'error',
+      title: 'Error',
+      text: 'No hay datos de combustibles disponibles.',
+      allowOutsideClick: false,
+    });
+    return;
+  }
+
+  if (!Array.isArray(storedCombustibles)) {
+    await Swal.fire({
+      icon: 'error',
+      title: 'Error',
+      text: 'Los datos de combustibles no están en el formato esperado.',
+      allowOutsideClick: false,
+    });
+    return;
+  }
+
+  // Encontrar el combustible seleccionado
+  const combustible = storedCombustibles.find(c => c.tipo === combustibleSeleccionado);
 
   if (!combustible) {
-    resultDiv.textContent = 'Tipo de combustible no encontrado';
+    await Swal.fire({
+      icon: 'error',
+      title: 'Error',
+      text: 'El tipo de combustible seleccionado no está disponible.',
+    });
     return;
   }
 
   let resultado = '';
-  const imagenDiv = document.createElement('div');
-  imagenDiv.className = 'image-container';
 
+  // Iterar sobre las estaciones de servicio
   combustible.eess.forEach(estacion => {
     const categoriasFiltradas = estacion.categorias.filter(categoria => categoria.precio <= monto);
-    
-    if (categoriasFiltradas.length > 0) {
-      categoriasFiltradas.forEach(categoria => {
-        const litrosEstacion = monto / categoria.precio;
-        resultado += `En ${estacion.nombre} puedes cargar ${litrosEstacion.toFixed(2)} litros de ${combustible.tipo} ${categoria.nombre}<br>`;
-      });
-    } else {
-      resultado += `En ${estacion.nombre} no hay categorías de ${combustible.tipo} dentro del rango de precio indicado.<br>`;
-    }
+    const tieneCategorias = categoriasFiltradas.length > 0;
 
-    // Crear y añadir la imagen de la estación de servicio
-    const estacionImage = document.createElement('img');
-    estacionImage.src = estacion.imagen;
-    estacionImage.alt = estacion.nombre;
-    imagenDiv.appendChild(estacionImage);
+    resultado += tieneCategorias ? categoriasFiltradas.map(categoria => {
+      const litrosEstacion = monto / categoria.precio;
+      return `En <strong>${estacion.nombre}</strong> puedes cargar ${litrosEstacion.toFixed(2)} litros de <strong>${combustible.tipo}</strong> (${categoria.nombre})<br>`;
+    }).join('') : `En <strong>${estacion.nombre}</strong> no hay categorías de <strong>${combustible.tipo}</strong> dentro del rango de precio indicado.<br>`;
   });
 
-  resultDiv.innerHTML = resultado;
-  resultDiv.appendChild(imagenDiv);
+  await Swal.fire({
+    title: 'Resultado',
+    html: resultado,
+    padding: `2rem`,
+    width: `30%`,
+    icon: 'info',
+    showCloseButton: true,
+    focusConfirm: false,
+    confirmButtonText: 'Aceptar',
+    allowOutsideClick: false,
+    imageUrl: `./assets/eess.png`,
+    imageWidth: `100%`,
+    imageHeight: 200,
+    imageAlt: "Estaciones de Servicio",
+  });
+  location.reload(); // refresca la página actual
 }
